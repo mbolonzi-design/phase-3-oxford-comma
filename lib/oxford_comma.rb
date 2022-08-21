81ff97ee3299005085ad837689a04415da679e09
mbolonzi-design <mbolonzi.design@gmail.com>
2022-08-21 07:35:19 +0300

    commit tree ac6b2575acc8a9807e8af2485811e4bb2bfdd83b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
+
+puts oxford_comma(["Brazil", "France", "Greece"])
